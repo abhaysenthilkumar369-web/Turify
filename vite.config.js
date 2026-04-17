3a944afc5a5be48b0a447d27310e2d1f21972311
abhay s <abhaysenthilkumar369@gmail.com>
2026-04-17 21:47:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/Turify/",   // must match your repo name exactly
 })
+
