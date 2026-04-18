07c7a9526ac01f1f99f9ec5cc8d39c553550539f
your-username <your-email>
2026-04-18 14:22:44 +0530

    Fix deployment: Fix package.json for building correctly on vercel and adapt vite.config.js base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Turify/",   // must match your repo name exactly
+  // use '/' for Vercel, and '/Turify/' for GitHub Pages
+  base: process.env.VERCEL ? "/" : "/Turify/",
 })
 
